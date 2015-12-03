e05e0496f695e91a43a7cb2b11ab7d0793558659
Fee <fee.fei@ucloud.cn>
2015-12-03 17:46:56 +0800

    change example cases

--- runCases.py
+++ runCases.py
@@ -35,7 +35,8 @@ if __name__ == '__main__':
     
     Run_Case = [
                 "test_homepage1.py",
-                "test_homepage2.py"
+                "test_homepage2.py",
+				"test_search1.py"
                 ]
     
     Run_Suite = "TestSuite01_Baidu_Example"
